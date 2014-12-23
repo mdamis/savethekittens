73f956eb6660b0e11790e7d223e3aa53fa51fc82
mdamis <mdamis@users.noreply.github.com>
2014-12-24 00:26:36 +0100

    Add jbox2d to Wall

--- src/elements/Wall.java
+++ src/elements/Wall.java
@@ -1,5 +1,32 @@
 package elements;
 
-public class Wall {
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
 
+public class Wall {
+	private static final float stantardWidth = 1.0f;
+	private static final float standardHeight = 1.0f;
+	
+	private Wall() {}
+	
+	public static void createStandardWall(World world, float x, float y) {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set(x, y);
+		bodyDef.type = BodyType.STATIC;
+		Body body = world.createBody(bodyDef);
+		createStandardWallFixtures(body);
+	}
+	
+	private static void createStandardWallFixtures(Body body) {
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(stantardWidth, standardHeight);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		body.createFixture(fixtureDef);
+	}
+	
 }
