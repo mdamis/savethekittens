938193530ce5bbe8e4a956ce11bd0c1a9fc50de6
mdamis <mdamis@users.noreply.github.com>
2014-12-30 22:48:40 +0100

    Add jbox2d to Bomb

--- src/elements/item/Bomb.java
+++ src/elements/item/Bomb.java
@@ -1,22 +1,55 @@
 package elements.item;
 
-public class Bomb implements Item {
-	private final int posX;
-	private final int posY;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
 
-	public Bomb(int posX, int posY) {
-		this.posX = posX;
-		this.posY = posY;
+import elements.cat.Cat;
+
+public class Bomb {
+	public static final String USER_DATA = "Bomb";
+	public static final int BIT_BOMB = 32;
+	public static final float RADIUS = 1.0f;
+	public static final float RANGE = 3.0f;
+	private final Body body;
+	private final int seconds;
+
+	private Bomb(Body body, int seconds) {
+		this.body = body;
+		this.seconds = seconds;
 	}
-
+	
+	public static Bomb create(World world, float x, float y, int seconds) {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set(x, y);
+		bodyDef.type = BodyType.STATIC;
+		Body body = world.createBody(bodyDef);
+		createFixtures(body);
+		Bomb bomb = new Bomb(body, seconds);
+		body.setUserData(bomb);
+		return bomb;
+	}
+	
+	private static void createFixtures(Body body) {
+		CircleShape shape = new CircleShape();
+		shape.setRadius(RANGE);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.filter.categoryBits = BIT_BOMB;
+		fixtureDef.filter.maskBits = Cat.BIT_CAT;
+		body.createFixture(fixtureDef).setUserData(USER_DATA);
+	}
+	
+	public Body getBody() {
+		return body;
+	}
+	
 	@Override
-	public int posX() {
-		return posX;
+	public String toString() {
+		return "Bomb : " + body.getPosition().x + " " + body.getPosition().y;
 	}
-
-	@Override
-	public int posY() {
-		return posY;
-	}
-
+	
 }
